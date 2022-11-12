923ed674b045a793ce53374a01c41be3601c93da
lopez-manuel <lopez.ledezma.manuel@gmail.com>
2022-11-11 18:58:40 -0600

    se agrego el html y css de menu productos y login para celular

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const products = document.getElementById('products');
+const productsMenu = document.querySelector('.products-menu')
+const mobileCartIcon = document.querySelector('.mobile-cart-icon')
+const mobileCartContainer = document.querySelector('.mobile-cart-container')
+
+
+products.addEventListener('click',toggleProductsMenu)
+products.addEventListener('mouseover',toggleProductsMenu)
+productsMenu.addEventListener('mouseleave',toggleProductsMenu)
+mobileCartIcon.addEventListener('click', toggleMobileCartContainer)
+
+function toggleProductsMenu(){
+    productsMenu.classList.toggle('inactive')
+}
+
+function toggleMobileCartContainer(){
+    mobileCartContainer.classList.toggle('inactive')
+}
