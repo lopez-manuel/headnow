f17b46917dac1c0f296e828fa03484b584ae0490
lopez-manuel <lopez.ledezma.manuel@gmail.com>
2022-11-15 00:17:43 -0600

    se rediseña el menu para mobile y se agrega barra de busqueda

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
 const products = document.getElementById('products');
-const productsMenu = document.querySelector('.products-menu')
-const mobileCartIcon = document.querySelector('.mobile-cart-icon')
-const mobileCartContainer = document.querySelector('.mobile-cart-container')
+const productsMenu = document.querySelector('.products-menu');
+const mobileCartIcon = document.querySelector('.mobile-cart-icon');
+const mobileCartContainer = document.querySelector('.mobile-cart-container');
+const navMenu = document.getElementById('nav-menu');
+const menuMobileButton = document.querySelector('.mobile-menu');
 
 
-products.addEventListener('click',toggleProductsMenu)
-//products.addEventListener('mouseover',toggleProductsMenu)
-productsMenu.addEventListener('mouseleave',toggleProductsMenu)
-mobileCartIcon.addEventListener('click', toggleMobileCartContainer)
+products.addEventListener('click',toggleProductsMenu);
+//products.addEventListener('mouseover',toggleProductsMenu);
+productsMenu.addEventListener('mouseleave',toggleProductsMenu);
+mobileCartIcon.addEventListener('click', toggleMobileCartContainer);
+menuMobileButton.addEventListener('click', toggleNavMenu);
 
 function toggleProductsMenu(){
-    productsMenu.classList.toggle('inactive')
+    productsMenu.classList.toggle('inactive');
+    navMenu.classList.remove('show-navbar-menu-container');
 }
 
 function toggleMobileCartContainer(){
-    mobileCartContainer.classList.toggle('inactive')
+    mobileCartContainer.classList.toggle('inactive');
+}
+
+function toggleNavMenu (){
+    navMenu.classList.toggle('show-navbar-menu-container');
 }
