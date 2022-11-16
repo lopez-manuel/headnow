85ed3e59578360898fca832017de5e840d1bfafe
lopez-manuel <lopez.ledezma.manuel@gmail.com>
2022-11-16 17:57:39 -0600

    se creo la pantalla de ordenes

--- js/ayuda.js
+++ js/ayuda.js
@@ -8,6 +8,8 @@ const shipping = document.getElementById('shipping')
 const shippingContainer = document.querySelector('.shipping-container')
 const refund = document.getElementById('refund')
 const refundContainer = document.querySelector('.refund-container')
+const account = document.getElementById('account')
+const accountContainer = document.querySelector('.account-container')
 
 
 
@@ -17,6 +19,7 @@ payments.addEventListener('click', showPaymentsContainer)
 prices.addEventListener('click', showPricesContainer)
 shipping.addEventListener('click', showShippingContainer)
 refund.addEventListener('click', showRefundContainer)
+account.addEventListener('click', showAccountContainer)
 
 
 
@@ -29,6 +32,7 @@ function showHowToBuyContainer(e){
     pricesContainer.classList.add('inactive')
     shippingContainer.classList.add('inactive')
     refundContainer.classList.add('inactive')
+    accountContainer.classList.add('inactive');
 }
 
 function showPaymentsContainer(e){
@@ -38,6 +42,7 @@ function showPaymentsContainer(e){
     pricesContainer.classList.add('inactive')
     shippingContainer.classList.add('inactive')
     refundContainer.classList.add('inactive')
+    accountContainer.classList.add('inactive');
 }
 
 function showPricesContainer(e){
@@ -47,6 +52,7 @@ function showPricesContainer(e){
     pricesContainer.classList.remove('inactive')
     shippingContainer.classList.add('inactive')
     refundContainer.classList.add('inactive')
+    accountContainer.classList.add('inactive');
 }
 
 function showShippingContainer(e){
@@ -56,13 +62,25 @@ function showShippingContainer(e){
     pricesContainer.classList.add('inactive')
     shippingContainer.classList.remove('inactive')
     refundContainer.classList.add('inactive')
+    accountContainer.classList.add('inactive');
 }
 
 function showRefundContainer(e){
     e.preventDefault()
-    howToBuyContainer.classList.add('inactive')
-    paymentsContainer.classList.add('inactive')
-    pricesContainer.classList.add('inactive')
-    shippingContainer.classList.add('inactive')
-    refundContainer.classList.remove('inactive')
+    howToBuyContainer.classList.add('inactive');
+    paymentsContainer.classList.add('inactive');
+    pricesContainer.classList.add('inactive');
+    shippingContainer.classList.add('inactive');
+    refundContainer.classList.remove('inactive');
+    accountContainer.classList.add('inactive');
+}
+
+function showAccountContainer(e){
+    e.preventDefault();
+    howToBuyContainer.classList.add('inactive');
+    paymentsContainer.classList.add('inactive');
+    pricesContainer.classList.add('inactive');
+    shippingContainer.classList.add('inactive');
+    refundContainer.classList.add('inactive');
+    accountContainer.classList.remove('inactive');
 }
