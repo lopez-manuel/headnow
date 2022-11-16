d1fa5e0bb32227bc541ea319bab53e02eeeed0aa
lopez-manuel <lopez.ledezma.manuel@gmail.com>
2022-11-16 00:37:04 -0600

    se agrego el icono de perfil y el menu en version mobile

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const mobileCartIcon = document.querySelector('.mobile-cart-icon');
 const mobileCartContainer = document.querySelector('.mobile-cart-container');
 const navMenu = document.getElementById('nav-menu');
 const menuMobileButton = document.querySelector('.mobile-menu');
+const mobileProfileIcon = document.querySelector('.mobile-profile-icon')
+const profileContainer = document.querySelector('.profile-container')
 
 
 products.addEventListener('click',toggleProductsMenu);
@@ -11,6 +13,7 @@ products.addEventListener('click',toggleProductsMenu);
 productsMenu.addEventListener('mouseleave',toggleProductsMenu);
 mobileCartIcon.addEventListener('click', toggleMobileCartContainer);
 menuMobileButton.addEventListener('click', toggleNavMenu);
+mobileProfileIcon.addEventListener('click', showProfileContainer)
 
 function toggleProductsMenu(){
     productsMenu.classList.toggle('inactive');
@@ -19,8 +22,14 @@ function toggleProductsMenu(){
 
 function toggleMobileCartContainer(){
     mobileCartContainer.classList.toggle('inactive');
+    profileContainer.classList.remove('profile-container-animation')
 }
 
 function toggleNavMenu (){
     navMenu.classList.toggle('show-navbar-menu-container');
+}
+
+function showProfileContainer(){
+    profileContainer.classList.toggle('profile-container-animation')
+    mobileCartContainer.classList.add('inactive')
 }
