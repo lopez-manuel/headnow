0864126450cae1756c7cb64771493fa2f868d395
lopez-manuel <lopez.ledezma.manuel@gmail.com>
2022-11-15 18:39:16 -0600

    se agrego la categoria de ayuda

---
+++ js/politicas.js
@@ -0,0 +1,87 @@
+const products = document.getElementById('products');
+const productsMenu = document.querySelector('.products-menu');
+const mobileCartIcon = document.querySelector('.mobile-cart-icon');
+const mobileCartContainer = document.querySelector('.mobile-cart-container');
+const navMenu = document.getElementById('nav-menu');
+const menuMobileButton = document.querySelector('.mobile-menu');
+const orders = document.getElementById('orders');
+const howToBuyContainer = document.querySelector('.how-to-buy-container')
+const payments = document.getElementById('payments');
+const paymentsContainer = document.querySelector('.payments-container')
+const prices = document.getElementById('prices');
+const pricesContainer = document.querySelector('.prices-container')
+const shipping = document.getElementById('shipping')
+const shippingContainer = document.querySelector('.shipping-container')
+const refund = document.getElementById('refund')
+const refundContainer = document.querySelector('.refund-container')
+
+products.addEventListener('click',toggleProductsMenu);
+//products.addEventListener('mouseover',toggleProductsMenu);
+productsMenu.addEventListener('mouseleave',toggleProductsMenu);
+//mobileCartIcon.addEventListener('click', toggleMobileCartContainer);
+menuMobileButton.addEventListener('click', toggleNavMenu);
+
+orders.addEventListener('click', showHowToBuyContainer)
+payments.addEventListener('click', showPaymentsContainer)
+prices.addEventListener('click', showPricesContainer)
+shipping.addEventListener('click', showShippingContainer)
+refund.addEventListener('click', showRefundContainer)
+
+
+function toggleProductsMenu(){
+    productsMenu.classList.toggle('inactive');
+    navMenu.classList.remove('show-navbar-menu-container');
+}
+
+function toggleMobileCartContainer(){
+    mobileCartContainer.classList.toggle('inactive');
+}
+
+function toggleNavMenu (){
+    navMenu.classList.toggle('show-navbar-menu-container');
+}
+
+function showHowToBuyContainer(e){
+    e.preventDefault()
+    howToBuyContainer.classList.remove('inactive')
+    paymentsContainer.classList.add('inactive')
+    pricesContainer.classList.add('inactive')
+    shippingContainer.classList.add('inactive')
+    refundContainer.classList.add('inactive')
+}
+
+function showPaymentsContainer(e){
+    e.preventDefault()
+    howToBuyContainer.classList.add('inactive')
+    paymentsContainer.classList.remove('inactive')
+    pricesContainer.classList.add('inactive')
+    shippingContainer.classList.add('inactive')
+    refundContainer.classList.add('inactive')
+}
+
+function showPricesContainer(e){
+    e.preventDefault()
+    howToBuyContainer.classList.add('inactive')
+    paymentsContainer.classList.add('inactive')
+    pricesContainer.classList.remove('inactive')
+    shippingContainer.classList.add('inactive')
+    refundContainer.classList.add('inactive')
+}
+
+function showShippingContainer(e){
+    e.preventDefault()
+    howToBuyContainer.classList.add('inactive')
+    paymentsContainer.classList.add('inactive')
+    pricesContainer.classList.add('inactive')
+    shippingContainer.classList.remove('inactive')
+    refundContainer.classList.add('inactive')
+}
+
+function showRefundContainer(e){
+    e.preventDefault()
+    howToBuyContainer.classList.add('inactive')
+    paymentsContainer.classList.add('inactive')
+    pricesContainer.classList.add('inactive')
+    shippingContainer.classList.add('inactive')
+    refundContainer.classList.remove('inactive')
+}
